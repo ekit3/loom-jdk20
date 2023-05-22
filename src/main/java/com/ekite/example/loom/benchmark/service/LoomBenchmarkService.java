d8ed1b3bc857a2c90a1fb7b880353458550ee8d4
maesb <maes.benjamin22@gmail.com>
2023-05-22 09:09:48 +0200

    [FEATURE] wip - benchmarks comparaison

---
+++ src/main/java/com/ekite/example/loom/benchmark/service/LoomBenchmarkService.java
@@ -0,0 +1,47 @@
+package com.ekite.example.loom.benchmark.service;
+
+import com.ekite.example.loom.benchmark.dto.BenchmarkResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoomBenchmarkService {
+
+    private final QueueSimulatorService queueSimulatorService;
+
+    public BenchmarkResult benchmark(Integer nbTask) {
+        BenchmarkResult br = BenchmarkResult.builder().name("loom").build();
+        br.start(nbTask);
+
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            br.setResult(new ArrayList<>());
+
+            List<Future<String>> futures = queueSimulatorService.generateRequests(nbTask)
+                    .stream()
+                    .map(req -> executor.submit(() -> queueSimulatorService.processRequest(req)))
+                    .toList();
+            // executor.close() is called implicitly, and waits
+
+
+            for (Future<String> future : futures) {
+                br.getResult().add(future.get());
+            }
+
+        } catch (Exception e) {
+            br.crashed(e.getMessage());
+            return br;
+        }
+
+        br.passed();
+        return br;
+    }
+}
--- src/test/java/com/ekite/example/loom/LoomApplicationTests.java
+++ src/test/java/com/ekite/example/loom/LoomApplicationTests.java
@@ -1,13 +1,73 @@
 package com.ekite.example.loom;
 
+import com.ekite.example.loom.benchmark.controller.BenchmarkController;
+import com.ekite.example.loom.benchmark.dto.BenchmarkResult;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SpringBootTest
+@Slf4j
 class LoomApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Autowired
+    private BenchmarkController controller;
+
+    @Test
+    void runBenchmarks() {
+        Arrays.asList(1, 10, 100, 1000, 1000000, 4000000).forEach(this::benchmark);
+    }
+
+    private void benchmark(Integer nbTask) {
+        int nbRun = 1;
+
+        List<BenchmarkResult> classicResults = new ArrayList<>();
+        List<BenchmarkResult> loomResults = new ArrayList<>();
+        List<BenchmarkResult> threadResults = new ArrayList<>();
+        List<BenchmarkResult> reactiveResults = new ArrayList<>();
+
+        IntStream.range(0, nbRun)
+                .forEach(run -> {
+                    classicResults.add(controller.classic(nbTask));
+                    loomResults.add(controller.loom(nbTask));
+                    threadResults.add(controller.thread(nbTask));
+                    StepVerifier.create(controller.reactive(nbTask))
+                            .expectNextMatches(reactiveResults::add)
+                            .verifyComplete();
+                });
+
+        computeResult(classicResults, nbTask);
+        computeResult(loomResults, nbTask);
+        computeResult(threadResults, nbTask);
+        computeResult(reactiveResults, nbTask);
+    }
+
+    private void computeResult(List<BenchmarkResult> results, Integer tasks) {
+        var name = results.stream().map(BenchmarkResult::getName).distinct().findFirst().orElse(null);
+        var elapsedTime = results.stream().map(BenchmarkResult::getElapsedTime).toList();
+        var minElapsedTime = Collections.min(elapsedTime);
+        var maxElapsedTime = Collections.max(elapsedTime);
+        var averageElapsedTime = elapsedTime.stream()
+                .mapToDouble(d -> d)
+                .average()
+                .orElse(0.0);
+
+        log.info("--- Computed Result for {} Benchmark with {} tasks ---", name, tasks);
+        log.info(" min elapsed time {} ms", minElapsedTime);
+        log.info(" max elapsed time {} ms", maxElapsedTime);
+        log.info(" average elapsed time {} ms", averageElapsedTime);
+        log.info("--------------------------------------------------------------");
+
+    }
 
 }
+
